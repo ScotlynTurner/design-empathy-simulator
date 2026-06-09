f7e6064e0de2bc951e844c156c0f8756d921e9c7
ScotlynTurner <st699@byu.edu>
2026-06-09 13:15:04 -0600

    feat: color blindness

--- scripts/demo.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById('sampleForm');
-const modalBackdrop = document.getElementById('modalBackdrop');
-document.getElementById('openModal').onclick = () => modalBackdrop.classList.add('open');
-document.getElementById('helpBtn').onclick = () => modalBackdrop.classList.add('open');
-document.getElementById('closeModal').onclick = () => modalBackdrop.classList.remove('open');
-modalBackdrop.onclick = (e) => { if (e.target === modalBackdrop) modalBackdrop.classList.remove('open'); };
-document.getElementById('hideCookie').onclick = () => document.getElementById('cookie').classList.add('hidden');
